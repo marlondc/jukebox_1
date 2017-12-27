94c812a367890f994b10bf00154665e98d309f3f
marlondc <mdatechong@gmail.com>
2017-12-26 21:24:18 -0500

    add postgres database

--- api/routes.js
+++ api/routes.js
@@ -3,8 +3,34 @@ const express = require('express');
 const dotenv = require('dotenv');
 const request = require('request');
 const R = require('ramda');
-const router = new express.Router();
+const Sequelize = require('sequelize');
+
 dotenv.config();
+
+const sequelize = new Sequelize('postgres://localhost/jukebox');
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+const User = sequelize.define('users', {
+  username: {
+    type: Sequelize.STRING
+  },
+  accessToken: {
+    type: Sequelize.STRING
+  },
+  refreshToken: {
+    type: Sequelize.STRING
+  }
+});
+
+const router = new express.Router();
 
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
@@ -17,20 +43,21 @@ const generateRandomString = N => (Math.random().toString(36)+Array(N).join('0')
 let access_token;
 let refresh_token;
 
+router.get('/', (req, res) => {
+  User.findAll().then(users => res.send({
+    tokens: users[0],
+  }));
+})
+
 router.get('/tokens', (req, res) => {
-  req.app.locals.db.collection('users').find().toArray((err, results) => {
-    if (err) throw err;
-    res.send({
-      tokens: results,
-    });
-  });
+  User.findAll().then(users => res.send({
+    tokens: users[0]
+  }));
 })
 
 router.get('/login', (req, res) => {
-  req.app.locals.db.collection('users').find().toArray((err, results) => {
-    if (results.length > 0) {
-      return res.redirect('/');
-    }
+  User.findAll().then((users) => {
+    if (users.length > 0) {return res.redirect('/tokens');};
     const state = generateRandomString(16);
     const queryString = querystring.stringify({
       response_type: 'code',
@@ -66,35 +93,45 @@ router.get('/callback', (req, res) => {
 
     request.post(authOptions, (error, response, body) => {
       if (!error && response.statusCode === 200) {
-        req.app.locals.db.collection('users').insert({
-          name: 'mdc268',
+        console.log(body.access_token);
+        User.create({
+          username: 'mdc268',
           accessToken: body.access_token,
           refreshToken: body.refresh_token,
-        });
+        }, { fields: ['username', 'accessToken', 'refreshToken'] }).then(() => {
+          console.log('hello');
+          res.redirect('/');
+        })
       }
-      res.redirect(process.env.REDIRECT_URL);
     });
   }
 });
 
 router.get('/refresh', function(req, res) {
-    const refresh_token = req.query.refreshToken;
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      headers: { 'Authorization': 'Basic ' + (new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')) },
-      form: {
-        grant_type: 'refresh_token',
-        refresh_token,
-      },
-      json: true
-    };
-  
-    request.post(authOptions, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        req.app.locals.db.collection('users').update({ name: 'mdc268' }, { $set: { accessToken: body.access_token } });
+  const refresh_token = req.query.refreshToken;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token,
+    },
+    json: true
+  };
+
+  request.post(authOptions, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      User.update({ accessToken: body.access_token }, { fields: ['accessToken'], where: { id: 4 } }).then(() => {
         res.send(body.access_token);
-      }
-    });
+      })
+    }
   });
+});
+
+router.get('/access', (req, res) => {
+  User.update({ accessToken: '1' }, { fields: ['accessToken'], where: { id: 4 } }).then(() => {
+    res.redirect('/');
+  })
+})
 
 module.exports = router;
